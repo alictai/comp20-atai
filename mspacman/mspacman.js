f14a80ea9d0e7b3bcdb183a50b4b0fab0fefb526
Alison Tai <alictai@gmail.com>
2013-02-13 20:18:31 -0500

    rendered blue game board to page

--- mspacman/mspacman.js
+++ mspacman/mspacman.js
@@ -4,10 +4,15 @@ function draw()
 	canvas = document.getElementById('bluegameboard');
     // Check if canvas is supported on browser
     if (canvas.getContext) {
-        ctx = canvas.getContext('2d');
-        img = new Image();
-    	img.src = 'pacman10-hp-sprite.png';
-    	ctx.drawImage(img, 0, 0);
+        context = canvas.getContext('2d');
+        blueboard = new Image();
+    	blueboard.src = 'pacman10-hp-sprite.png';
+    	//drawing the board
+    	src_x = 322;
+    	src_y = 0;
+    	src_w = 464;
+    	src_h = 137;
+    	context.drawImage(blueboard, src_x, src_y, src_w, src_h, 0, 0, src_w, src_h);
     }
     else {
         alert('Sorry, canvas is not supported on your browser!');
