7b4cbd5ff5291a918c492d18d0486f933c9038c0
Alison Tai <alictai@gmail.com>
2013-02-14 03:35:30 -0500

    drew water and road

--- frogger/game.js
+++ frogger/game.js
@@ -1,1 +1,42 @@
-// Your work goes here...
+//initialized global variables
+frog_start_x = 0;
+frog_start_y = 0;
+num_lives = 3;
+game_over = false;
+num_lvl = 1;
+time = 120;
+car_speed = 5; 
+log_speed = 5;
+canvas_h = 565;
+canvas_w = 399;
+
+function start_game()
+{	
+	console.log("game starting");
+	draw_graphics();
+}
+
+function draw_graphics()
+{
+	console.log("drawing graphics");
+	canvas = document.getElementById('game');
+	if (canvas.getContext) {
+		context = canvas.getContext('2d');
+		
+		//Draw water
+		context.beginPath();
+      	context.rect(0, 0, canvas_w, canvas_h/2);
+    	context.fillStyle = '#191970';
+    	context.fill();
+    	
+    	//Draw road
+    	context.beginPath();
+		context.rect(0, canvas_h/2, canvas_w, canvas_h/2);
+		context.fillStyle = '#000000';
+		context.fill();
+
+	}
+	else {
+		alert('Sorry, canvas is not supported on your browser!');
+	}
+}
