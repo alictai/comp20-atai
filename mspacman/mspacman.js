dae62281a7419d5a9e8224daf957b6e3230f9d92
Alison Tai <alictai@gmail.com>
2013-02-14 01:46:35 -0500

    Added Ms. Pac-Man and blue ghost sprites

--- mspacman/mspacman.js
+++ mspacman/mspacman.js
@@ -1,18 +1,36 @@
 function draw()
 {
 	console.log("YAY I'M DOING STUFF");
-	canvas = document.getElementById('bluegameboard');
+	canvas = document.getElementById('gameboard');
     // Check if canvas is supported on browser
     if (canvas.getContext) {
         context = canvas.getContext('2d');
-        //drawing the board
-        blueboard = new Image();
-    	blueboard.src = 'pacman10-hp-sprite.png';
+        
+        sprites = new Image();
+    	sprites.src = 'pacman10-hp-sprite.png';
+
+		//drawing the board
     	src_x = 322;
     	src_y = 0;
     	src_w = 464;
     	src_h = 137;
-    	context.drawImage(blueboard, src_x, src_y, src_w, src_h, 0, 0, src_w, src_h);
+    	context.drawImage(sprites, src_x, src_y, src_w, src_h, 0, 0, src_w, src_h);
+    	
+    	//drawing Ms. Pac-Man
+    	src_x = 84;
+    	src_y = 3;
+    	src_w = 13;
+    	src_h = 14;
+    	context.drawImage(sprites, src_x, src_y, src_w, src_h, 360, 7, src_w, src_h);
+
+		//drawing blue ghost
+		src_x = 43;
+		src_y = 123;
+		src_w = 14
+		src_h = 14
+   		context.drawImage(sprites, src_x, src_y, src_w, src_h, 245, 20, src_w, src_h);
+
+    	
     }
     else {
         alert('Sorry, canvas is not supported on your browser!');
